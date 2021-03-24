9055c2b4f44de3126ed0bb7adc019ce49512af5b
Erriep <tabary.pierre.dev@gmail.com>
2021-03-24 16:30:39 +0100

    Add JS on miniature + correction css

---
+++ produitManteauHomme.js
@@ -0,0 +1,9 @@
+const mini1 = document.querySelector("#miniManteauHomme1");
+const mini2 = document.querySelector("#miniManteauHomme2");
+const mini3 = document.querySelector("#miniManteauHomme3");
+const imageProduit = document.querySelector(".img")
+
+
+mini1.addEventListener('click', ()=> {img.src = "img/manteau.JPG"});
+mini2.addEventListener('click', ()=> {img.src = "img/manteau2.JPG"});
+mini3.addEventListener('click', ()=> {img.src = "img/manteau3.JPG"});
